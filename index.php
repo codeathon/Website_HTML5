09e1d51462a830182a6a0b6be932c5188d3763b2
unknown <Ragster@Ragster-PC.(none)>
2012-06-19 10:58:17 -0700

    .

--- index.php
+++ index.php
@@ -85,12 +85,50 @@
 					  <p><span class="clear"><a href="etest/test.php">Extra Test</a></span></p>
 					</center>
 
-					<a href="hiv_aids_home_test.php"></a>
+					<a href="test.php"></a>
 
 				</div>
 			</div>
 		</div>
   </div>
+  
+      <div class="grid_2 prefix_1 suffix_2">
+		<div class="lesson_label">
+			<div class="shadow1">	
+				<div class="content" style="cursor:pointer">
+					<img alt="logo" src="images/aids-ribbon.png" id="label2" height="130" width="140">
+					<br/> <center> 
+					  <p><b> HIV / AIDS Demo 4</b>
+				      </p>
+					  <p><span class="clear"><a href="aids/load_lesson_R.php">Extra Test</a></span></p>
+					</center>
+
+					<a href="test.php"></a>
+
+				</div>
+			</div>
+		</div>
+  </div>
+  
+      <div class="grid_2 prefix_1 suffix_2">
+		<div class="lesson_label">
+			<div class="shadow1">	
+				<div class="content" style="cursor:pointer">
+					<img alt="logo" src="images/aids-ribbon.png" id="label2" height="130" width="140">
+					<br/> <center> 
+					  <p><b> HIV / AIDS Demo 5</b>
+				      </p>
+					  <p><span class="clear"><a href="etest1/test.php">Extra Test</a></span></p>
+					</center>
+
+					<a href="test.php"></a>
+
+				</div>
+			</div>
+		</div>
+  </div>
+  
+  
   
 <!-- The following JQuery is used to fetch the anchor tag in the div element and redirect the browser to that location. -->
   
